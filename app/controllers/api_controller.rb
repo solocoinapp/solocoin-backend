2c66599bfea6f83cd49ab776278f03ee705f27dd
Prasad Pilla <prasad.pilla81@gmail.com>
2020-03-27 16:19:02 +0530

    Implement API's for Firebase Auth
    * Multi provider OAuth login Ex: Google, Faccebook, Mobile, Email
    * Firebase mobile number login (OTP verification)
    * Firebase Push notifications
    * AppSignal for monitoring
    * MSG91 for sending SMS (INDIA)

--- app/controllers/api_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApiController < ApplicationController
-  def validate_session
-    # Validate user from session
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.find_or_initialize_by(email: "admin@coronagoapp.com")
+unless user.persisted?
+  user.password = 'CoronaGo$3'
+  user.name = 'CoronaGo Admin'
+  user.confirmed_at = DateTime.now
+  user.country_code = "+91"
+  user.mobile = "8866553322"
+  user.save!
+end
