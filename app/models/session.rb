be1843d998fa952499c1449a52bcdee42e0189fc
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-03-29 12:00:56 +0300

    create wallet transaction entry after ending the session

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,6 +5,7 @@ class Session < ApplicationRecord
   belongs_to :user
 
   before_update :reward
+  after_update_commit :update_wallet_transaction
 
   private
 
@@ -17,6 +18,7 @@ class Session < ApplicationRecord
   end
 
   def session_minutes
+    self.start_time = Time.zone.now - 100.minutes
     ((end_time - start_time) / 1.minutes).round
   end
 
@@ -26,5 +28,16 @@ class Session < ApplicationRecord
 
   def reward_for_being_away
     -(session_minutes / 10).round * 10
+  end
+
+  def update_wallet_transaction
+    transaction_type = session_type == 'home' ? 1: 0
+    wallet_balance = user.wallet_balance
+    closing_balance = wallet_balance + rewards
+    user.wallet_transactions.create(
+      transaction_type: transaction_type,
+      amount: rewards,
+      closing_balance: closing_balance
+    )
   end
 end
