5b101cd4c29c934a1db08fd3eecd99b4807b13b3
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-03-29 10:43:36 +0300

    remove active session column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_071809) do
+ActiveRecord::Schema.define(version: 2020_03_29_053156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,7 +104,6 @@ ActiveRecord::Schema.define(version: 2020_03_29_071809) do
     t.string "identifier"
     t.decimal "lat", precision: 10, scale: 6
     t.decimal "lng", precision: 10, scale: 6
-    t.bigint "active_session"
     t.index ["auth_token"], name: "index_users_on_auth_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email"
