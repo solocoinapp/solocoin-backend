f26f64499bb0a40a06290f20c7610a0771b00fa3
Prasad Pilla <prasad.pilla81@gmail.com>
2020-03-31 12:50:56 +0530

    Avoid record save for existing tokens
    Co-Authored-By: John Tzikas <nronas@gmail.com>

--- app/controllers/api/v1/notification_tokens_controller.rb
+++ app/controllers/api/v1/notification_tokens_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::NotificationTokensController < Api::BaseController
 
   def create
-    @notification_token = current_user.notification_tokens.find_or_initialize_by(value: notification_token_params[:token])
-    if @notification_token.save
+    @notification_token = current_user.notification_tokens.find_or_create_by(value: notification_token_params[:token])
+    if @notification_token.valid?
       render json: {}, status: :created
     else
       render_error(:unprocessable_entity, @notification_token.errors.full_messages.to_sentence)
