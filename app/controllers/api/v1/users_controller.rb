7930d29e49acd2f258f4c9ff66c417733b8e4aaf
nronas <nronas@gmail.com>
2020-03-31 21:50:26 +0100

    Remove left over action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,23 +11,9 @@ class Api::V1::UsersController < Api::BaseController
     end
   end
 
-  def register_notification_token
-    @notification_token = current_user.notification_tokens.find_or_create_by(value: notification_token_params[:token])
-
-    if @notification_token.valid?
-      render json: {}, status: :created
-    else
-      render_error(:unprocessable_entity, @notification_token.errors.full_messages.to_sentence)
-    end
-  end
-
   private
 
   def user_params
     params.require(:user).permit(:name, :mobile, :profile_picture, :lat, :lng)
-  end
-
-  def notification_token_params
-    params.require(:user).permit(:token)
   end
 end
