7ac9fe9ace32eb6b640328ab5c5bf0b87870cfd8
nronas <nronas@gmail.com>
2020-03-31 23:30:34 +0100

    Refactor LeaderBoard flow and cover with request specs

--- app/controllers/api/v1/leaderboards_controller.rb
+++ app/controllers/api/v1/leaderboards_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::LeaderboardsController < Api::BaseController
-
   def show
-    render json: User.fetch_leaderboard_stats(current_user)
+    render json: LeaderBoard.new(user: current_user)
   end
 end
