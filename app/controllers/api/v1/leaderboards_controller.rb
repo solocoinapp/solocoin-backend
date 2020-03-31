fb232ca283cab3d52a72390965c9c42d04f04520
Prasad Pilla <prasad.pilla81@gmail.com>
2020-03-31 13:28:27 +0530

    Fix failing leaderboard endpoint

--- app/controllers/api/v1/leaderboards_controller.rb
+++ app/controllers/api/v1/leaderboards_controller.rb
@@ -1,16 +1,6 @@
 class Api::V1::LeaderboardsController < Api::BaseController
-  before_action :set_user, only: :show
-  before_action :check_ownership, only: :show
 
   def show
-    response = User.fetch_leaderboard_stats(@user)
-    render json: response
+    render json: User.fetch_leaderboard_stats(current_user)
   end
-
-  private
-
-  def set_user
-    @user = User.find_by!(id: params[:id])
-  end
-
 end
