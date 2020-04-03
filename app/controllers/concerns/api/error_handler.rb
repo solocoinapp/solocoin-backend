a440bce630da8e33c50f80f38dc46cdc45f3426b
Prasad Pilla <prasad.pilla81@gmail.com>
2020-04-04 00:59:13 +0530

    Fix hard fail while reporting 500 errors for non authenticated requests

--- app/controllers/concerns/api/error_handler.rb
+++ app/controllers/concerns/api/error_handler.rb
@@ -28,8 +28,8 @@ module Api
     end
 
     def internal_server_error(exception)
-      log_and_report(exception, {params: params.to_json, request_body: request.body.to_json,
-                                 current_user_id: current_user.id}, 'web')
+      log_and_report(exception, {params: params.try(:to_json), request_body: request.try(:body).try(:to_json),
+                                 current_user_id: current_user.try(:id)}, 'web')
       render_error(:internal_server_error, I18n.t('general.something_went_wrong'))
     end
 
