02a15a4b30015373d449a4f3bf4279d94115a9b4
Prasad Pilla <prasad.pilla81@gmail.com>
2020-04-04 23:01:45 +0530

    Enable Sidekiq on QA01

--- config/deploy/qa01.rb
+++ config/deploy/qa01.rb
@@ -2,4 +2,4 @@ set :stage, :qa01
 set :rails_env, :production
 set :log_level, :debug
 server '52.78.25.78', user: 'deploy', roles: %w{app db web}
-set :sidekiq_processes, 0
+set :sidekiq_processes, 1
