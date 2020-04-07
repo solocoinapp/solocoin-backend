c47cd82afbccba30d32588ee2787de8d47155833
Prasad Pilla <prasad.pilla81@gmail.com>
2020-04-07 15:58:28 +0530

    Add lat, lng fields in user response

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :mobile, :profile_picture_url, :wallet_balance,
-             :home_duration_in_seconds, :away_duration_in_seconds
+             :home_duration_in_seconds, :away_duration_in_seconds, :lat, :lng
 end
