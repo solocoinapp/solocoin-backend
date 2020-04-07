1eb27a70214caef8b7ca5caa8e7cb4acc8ef57af
Prasad Pilla <prasad.pilla81@gmail.com>
2020-04-08 01:01:15 +0530

    Update redis engine and parameter group versions

--- infra/tf/redis/instance.tf
+++ infra/tf/redis/instance.tf
@@ -3,6 +3,6 @@ resource "aws_elasticache_cluster" "solocoinapp_redis" {
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  parameter_group_name = "default.redis5.0"
+  engine_version       = "5.0.6"
 }
