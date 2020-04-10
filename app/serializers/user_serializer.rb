d31872f1ece5a992e0f8ba9dc5f2ae0e67da9af5
Prasad Pilla <prasad.pilla81@gmail.com>
2020-04-10 17:30:02 +0530

    Return reltime wallet balance including active session

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -5,9 +5,18 @@ class UserSerializer < ActiveModel::Serializer
   def home_duration_in_seconds
     active_session = object.active_session
     if active_session.present? && active_session.home?
-      object.home_duration_in_seconds + active_session.in_seconds
+      object.home_duration_in_seconds + Sessions::Duration.in_seconds(active_session)
     else
       object.home_duration_in_seconds
+    end
+  end
+
+  def wallet_balance
+    active_session = object.active_session
+    if active_session.present? && active_session.home?
+      object.wallet_balance + Sessions::Rewards.calculate('home', Sessions::Duration.in_minutes(active_session))
+    else
+      object.wallet_balance
     end
   end
 end
