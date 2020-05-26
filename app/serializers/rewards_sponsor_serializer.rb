8b2f78c3f3b99b641512b9b41e62a750cc232b6c
MohdAnas <mohd.anas89@yahoo.com>
2020-05-27 00:17:25 +0530

    Added offer amount to rewards API

--- app/serializers/rewards_sponsor_serializer.rb
+++ app/serializers/rewards_sponsor_serializer.rb
@@ -1,3 +1,3 @@
 class RewardsSponsorSerializer < ApplicationSerializer
-  attributes :id, :offer_name, :company_name, :terms_and_conditions, :coins, :coupon_code
+  attributes :id, :offer_name, :offer_amount, :company_name, :terms_and_conditions, :coins, :coupon_code
 end
