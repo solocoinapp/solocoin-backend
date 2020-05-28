7daaba5213f3dc69bf3180e4ed13b84e66bc6f53
MohdAnas <mohd.anas89@yahoo.com>
2020-05-28 22:56:26 +0530

    added min_points to badge API

--- app/serializers/badge_serializer.rb
+++ app/serializers/badge_serializer.rb
@@ -1,5 +1,5 @@
 class BadgeSerializer < ApplicationSerializer
-  attributes :level, :name, :one_liner, :badge_image_url
+  attributes :level, :name, :one_liner, :badge_image_url, :min_points
 
   def badge_image_url
     object.badge_image.url
