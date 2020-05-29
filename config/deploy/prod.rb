1a6cc37146834356662d1841c6dd38ed0283ccda
MohdAnas <mohd.anas89@yahoo.com>
2020-05-29 13:10:25 +0530

    setting ssh keys to capistrano

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -3,8 +3,4 @@ set :rails_env, :production
 set :log_level, :debug
 server 'ec2-13-233-75-179.ap-south-1.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}
 set :sidekiq_processes, 1
-ssh_options: {
-    keys: %w("/home/mohd/Downloads/solocoin_aws_ec2_keys/solocoin_aws_ec2_keys/Prod/prod_16_apr_20.pem"),
-    forward_agent: false,
-    auth_methods: %w(publickey)
-  }
+set :ssh_options, {:forward_agent => true}
