2e209b92e7df2cbc32bb9232d414b4513c156d33
MohdAnas <mohd.anas89@yahoo.com>
2020-05-29 13:19:16 +0530

    set repo in  cap file

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,6 +1,7 @@
 set :stage, :prod
 set :rails_env, :production
 set :log_level, :debug
+set :repository, "git@github.com:solocoinapp/solocoin-backend.git"
 server 'ec2-13-233-75-179.ap-south-1.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}
 set :sidekiq_processes, 1
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
