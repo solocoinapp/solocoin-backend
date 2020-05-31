c6490c00248acbbbc4d7a4e9a575a52748f987ab
MohdAnas <mohd.anas89@yahoo.com>
2020-05-31 13:17:44 +0530

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Admin User creation
-User.create(name: 'Admin user', email: 'admin@admin.com', password: '!QAZ2wsx', password_confirmation:'!QAZ2wsx', is_admin: true)
+User.create(name: 'Admin user', email: 'admin111@admin.com', password: '!QAZ2wsx', password_confirmation:'!QAZ2wsx', role: 1)
