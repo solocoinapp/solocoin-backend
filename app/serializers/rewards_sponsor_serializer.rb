8c2a4acea3b9f8970ce34f47cdd901f06849795f
Sujay Gavhane <sujay.gavhane@gmail.com>
2020-08-22 19:13:02 +0530

    Added Currency type field in RewardsSponsor

--- app/serializers/rewards_sponsor_serializer.rb
+++ app/serializers/rewards_sponsor_serializer.rb
@@ -1,5 +1,5 @@
 class RewardsSponsorSerializer < ApplicationSerializer
-  attributes :id, :offer_name, :offer_amount, :company_name, :terms_and_conditions, :coins, :coupon_code, :brand_logo_url
+  attributes :id, :offer_name, :offer_amount, :company_name, :terms_and_conditions, :coins, :coupon_code, :brand_logo_url, :currency_type
   has_one :category, serializer: CategorySerializer
 
   def brand_logo_url
