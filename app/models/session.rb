e3fb239f5afe5e5c4d1a9db879b85b339f030639
Sujay Gavhane <sujay.gavhane@gmail.com>
2020-09-08 17:30:02 +0530

    Changed PING_TIMEOUT_IN_MINUTES to 60 minutes

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,7 +2,7 @@ class Session < ApplicationRecord
   enum status: { 'in_progress': 0, done: 1 }
   enum session_type: { home: 0, away: 1 }
 
-  PING_TIMEOUT_IN_MINUTES = 30
+  PING_TIMEOUT_IN_MINUTES = 60
 
   belongs_to :user
 
